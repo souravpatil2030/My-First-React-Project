433c7a62ce4e59b5656667fb9342a84442ecf026
souravp13062001 <116303138+souravp13062001@users.noreply.github.com>
2022-10-20 23:23:18 +0530

    Create firreactrules.py

---
+++ firreactrules.py
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class RulesPage extends StatefulWidget {
+  @override
+  _RulesPageState createState() => _RulesPageState();
+}
+
+class _RulesPageState extends State<RulesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Scratch and Win"),
+      ),
+      body: Center(
+        child: Text("The rules for this game are pretty simple"),
+	      child: Text("• Newbie project created in react"),
+	      child: Text("• React is only for professional web developers"),
+	      child: Text("• only for fun"),
+      ),
+    );
+  }
+}
